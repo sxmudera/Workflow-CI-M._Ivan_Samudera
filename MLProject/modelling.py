3992c45e0482a8ab879dbb52926543d81f2694c2
ivan <ivansamudera22@gmail.com>
2025-12-23 16:13:12 +0700

    fix

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,7 +2,6 @@ import pandas as pd
 import mlflow
 import dagshub
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 import joblib
 import os
 
@@ -12,11 +11,11 @@ if token:
 
 dagshub.init(repo_owner='sxmudera', repo_name='Heart-Failure-Project', mlflow=True)
 
+train = pd.read_csv('train_data.csv')
+X_train = train.drop('DEATH_EVENT', axis=1)
+y_train = train['DEATH_EVENT']
+
 with mlflow.start_run():
-    train = pd.read_csv('train_data.csv')
-    X_train = train.drop('DEATH_EVENT', axis=1)
-    y_train = train['DEATH_EVENT']
-    
     model = RandomForestClassifier(n_estimators=100)
     model.fit(X_train, y_train)
     
